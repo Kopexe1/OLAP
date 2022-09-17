9327916ba405f9ed5ad9c3fbea4006a3d03c70ca
Kopexe1 <55954063+Kopexe1@users.noreply.github.com>
2022-09-17 13:39:26 +0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo 'create database dot; use dot; CREATE TABLE words(`id` int primary key not null ,`field1` varchar(40),`field2` varchar(40),`field3` varchar(40),`field4` varchar(40),`field5` varchar(40),`field6` varchar(40),`field7` varchar(40),`field8` varchar(40),`field9` varchar(40),`field10` varchar(40),`field11` varchar(40),`field12` varchar(40),`field13` varchar(40),`field14` varchar(40),`field15` varchar(40),`field16` varchar(40),`field17` varchar(40),`field18` varchar(40),`field19` varchar(40),`field20` varchar(40));' | mysql -u root -p
+
